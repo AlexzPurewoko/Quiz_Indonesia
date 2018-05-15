f0e989867bf77bdfc1b2ad82b47200253275a359
AlexzPurewoko <purwoko908@gmail.com>
2018-05-15 17:00:50 +0700

    Set to Fullscreen

--- app/src/main/java/com/mylexz/app/quiz/MainActivity.java
+++ app/src/main/java/com/mylexz/app/quiz/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.support.v7.appcompat.R.style.Theme_AppCompat_Light_NoActionBar
     }
 }
